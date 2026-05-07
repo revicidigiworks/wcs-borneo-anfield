809557011d419cde7b2b7c8507bf11cbff24d31b
byrafx <muhammadrafli.tawil@gmail.com>
2026-05-07 10:19:14 +0800

    fix login admin

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
-  const isAdmin = localStorage.getItem("admin");
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../services/firebase";
 
-  if (!isAdmin) return <Navigate to="/admin" />;
+export default function ProtectedRoute({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (currentUser) => {
+        setUser(currentUser);
+        setLoading(false);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-black text-white">
+        Loading...
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/admin" replace />;
+  }
 
   return children;
 }
