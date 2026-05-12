02377ff3324eefa296ef7f20e9ca05b20bac250f
byrafx <muhammadrafli.tawil@gmail.com>
2026-05-12 10:42:57 +0800

    update pdf crop gambar

--- src/utils/exportPDF.js
+++ src/utils/exportPDF.js
@@ -99,26 +99,8 @@ const toBase64 = async (url) => {
             height
           );
 
-          const targetRatio = 20 / 26;
-
-          let cropWidth = width;
-          let cropHeight = height;
-
-          if (width / height > targetRatio) {
-            cropWidth = height * targetRatio;
-          } else {
-            cropHeight = width / targetRatio;
-          }
-
-          const sx = (width - cropWidth) / 2;
-          const sy = (height - cropHeight) / 2;
-
           ctx.drawImage(
             img,
-            sx,
-            sy,
-            cropWidth,
-            cropHeight,
             0,
             0,
             width,
@@ -680,7 +662,7 @@ export const exportTeamsPDF =
       orientation: "p",
       unit: "mm",
       format: "a4",
-
+      
     });
 
     const list =
@@ -776,7 +758,7 @@ export const exportTeamsPDF =
       const teamName =
         sanitizeFileName(
           teams?.name ||
-          "TEAM"
+            "TEAM"
         );
 
       doc.save(
