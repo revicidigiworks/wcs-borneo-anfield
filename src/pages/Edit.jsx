43b1b189814c973fef087bfb37dfb26541f9f693
byrafx <muhammadrafli.tawil@gmail.com>
2026-06-06 12:30:52 +0800

    age 15 smntara

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -225,7 +225,7 @@ export default function EditTeam() {
 
       const age = calcAge(p.dob);
 
-      if (age < 15) {
+      if (age < 16) {
         alert(`Usia pemain ke-${i + 1} minimal 16 tahun`);
         return false;
       }
